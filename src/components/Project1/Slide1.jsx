ed2cd6bae175040a0ecbee624d12a73b1683e6e1
Akash <akashbalcham@gmail.com>
2025-02-01 17:40:02 +0530

    fix: slide 2 && 3 responsive

--- src/components/Project1/Slide1.jsx
+++ src/components/Project1/Slide1.jsx
@@ -4,25 +4,27 @@ import p1_1 from "../../assets/images/p1_1.png"
 
 const Slide1 = () => {
     return (
-        <div className="w-full flex pl-64 pr-40 pt-24 flex-row justify-start">
-            <img src={p1_1} className="h-[73vh] w-[31vw]" alt="" />
-            <div className={`w-[39%] ml-28 flex flex-col items-start`}>
+        <div className="w-full flex pl-64 lg:pl-20 pr-40 pt-24 lg:pt-16 2xl:pl-28 4xl:pl-36 flex-row justify-start">
+            <img src={p1_1} className="h-[73vh] w-[31vw] lg:h-[70vh] lg:w-[40vw] lgMid:w-[37vw] lgMid:h-[73vh] 2xl:w-[35vw] 4xl:h-[75vh]" alt="" />
+            <div className={`w-[39%] ml-28 lg:ml-12 lgMid:ml-16 xl:ml-24 flex flex-col items-start`}>
                 <div className={` flex flex-col items-start justify-evenly `}>
-                    <h1 className="font-canela text-5xl italic font-semibold tracking-wide">The Inspiration</h1>
+                    <h1 className="font-canela text-5xl lg:text-4xl lgMid:text-[280%] xl:text-5xl 2xl:text-[300%] italic font-semibold tracking-wide 4xl:text-6xl">The Inspiration</h1>
                     
-                    <h2 className="font-duplet mt-2 tracking-tight font-bold text-4xl text-[#212880]">2024 Elections</h2>
+                    <h2 className="font-duplet mt-2 lg:mt-1 tracking-tight font-bold lg:text-3xl lgMid:text-[210%] text-4xl xl:text-4xl  2xl:text-[270%] 4xl:text-[290%] text-[#212880]">2024 Elections</h2>
                 </div>
 
-                <div className="-mt-4 w-[102%] font-gilroy h-full flex flex-col items-start justify-around">
-                    <p className="text-[28px] leading-relaxed">During the 2024 elections, 
-                        I stood in line at the polling booth, overhearing people ask, 
-                        <span className="font-semibold">“Do you know who the candidates are? What have they done?” </span>
-                        Another replied,  “I only know the party names; 
-                        I have no idea about the candidates themselves?”.
+                <div className="lg:mt-6 font-gilroy h-full flex flex-col items-start justify-around">
+                    <p className="text-[28px] lg:text-[90%] lg:leading-[175%] lgMid:text-[114%] lgMid:leading-[190%] xl:text-[130%] xl:leading-[190%] xlMid:text-[140%] xlMid:leading-[190%] 2xl:text-[150%] 2xl:leading-10 3xl:text-[175%] 3xl:leading-relaxed 4xl:text-[200%]"
+                    
+                    >During the 2024 elections, 
+                        I stood in line at <br/> the polling booth, overhearing people ask, <br/>
+                        <span className="font-semibold">“Do you know who the candidates are? <br/> What have they done?” </span>
+                        Another replied, “I <br/>only know the party names; 
+                        I have no idea <br/> about the candidates themselves?”.
                     </p>
-                    <p className="-mt-20 text-[28px] leading-relaxed">It struck me because I was just as uninformed, 
-                        scrambling for details <span className="font-semibold">the day before but finding 
-                        scattered, unhelpful information.</span>
+                    <p className="-mt-20 lg:-mt-4 lgMid:-mt-2 xl:mt-0 lg:text-[90%] leading-[175%] lgMid:text-[114%] lgMid:leading-[190%] xl:text-[130%] xl:leading-[190%] xlMid:text-[140%] 2xl:text-[150%] 2xl:leading-10 3xl:text-[175%] 3xl:leading-relaxed 4xl:text-[200%]">It struck me because I was just as <br/>uninformed, 
+                        scrambling for details <span className="font-semibold">the day<br/> before but finding 
+                        scattered, unhelpful<br/> information.</span>
                     </p>
                 </div>
 
--- src/components/Project1/Slide2.jsx
+++ src/components/Project1/Slide2.jsx
@@ -4,29 +4,29 @@ import p1_2 from "../../assets/images/p1_2.png"
 
 const Slide2 = () => {
     return (
-        <div className="w-full flex pl-64 pr-40 pt-24 flex-row justify-start">
-            <img src={p1_2} className="h-[73vh] w-[31vw]" alt="" />
-            <div className={`w-[39%] ml-28 flex flex-col items-start justify-between`}>
+        <div className="w-full flex lg:pl-24 xlMid:pl-32 4xl:pl-40 pl-64 pr-40 lg:pt-16 xlMid:pt-20 4xl:pt-24  pt-24 flex-row justify-start">
+            <img src={p1_2} className="h-[73vh] w-[31vw] lg:w-[40vw] xlMid:w-[38vw] 4xl:w-[35vw]" alt="" />
+            <div className={`w-[39%] lg:ml-16 ml-28 4xl:ml-28 flex flex-col items-start justify-between`}>
                 <div className={` flex flex-col items-start justify-evenly `}>
-                    <h1 className="font-canela text-5xl italic font-semibold tracking-wide">The Inspiration</h1>
+                    <h1 className="font-canela text-5xl lg:text-4xl lgMid:text-[280%] xl:text-5xl 2xl:text-[350%] 3xl:text-[380%] italic font-semibold tracking-wide 4xl:text-6xl">The Inspiration</h1>
                     
-                    <h2 className="font-duplet mt-2 tracking-tight font-bold text-4xl text-[#212880]">2024 Elections</h2>
+                    <h2 className="font-duplet mt-2 lg:mt-1 tracking-tight font-bold lg:text-3xl lgMid:text-[210%] text-4xl xl:text-4xl  2xl:text-[270%] 3xl:text-[280%] 4xl:text-[290%] text-[#212880]">2024 Elections</h2>
                 </div>
 
                 
 
-                <div className="mt-24 w-[80%] font-gilroy h-full flex flex-col items-start justify-start">
-                    <p className="text-[28px] w-[110%] leading-10 mb-4">In the 2024 Indian General Election, <br/> 
-                        <span className="font-semibold">46% of elected MPs</span> declared criminal 
+                <div className="mt-24 lg:mt-20 2xl:mt-16 font-gilroy h-full flex flex-col items-start justify-start">
+                    <p className="text-[28px] lg:text-[125%] lgMid:text-[130%] xl:text-[150%] xlMid:text-[165%] xlMid:leading-10 2xl:text-[180%] 2xl:leading-[180%] 3xl:text-[200%] 4xl:text-[200%] 4xl:leading-[180%] w-[110%] lg:leading-9 leading-10 lg:mb-2 2xl:mb-2 mb-4">In the 2024 Indian General Election, <br/> 
+                        <span className="font-semibold">46% of elected MPs</span> declared criminal <br/> 
                         cases against them. 
                     </p>
-                    <p className="text-[28px] leading-10"><span className="font-semibold text-red-600">31% facing serious charges</span> such as 
-                            murder, attempted murder, or crimes 
+                    <p className="lg:text-[120%] lg:leading-9 lgMid:text-[130%] xl:text-[150%] w-[110%]  xlMid:text-[165%] xlMid:leading-10 2xl:text-[180%] 2xl:leading-[180%] leading-10 4xl:text-[200%] 4xl:leading-[180%]"><span className="font-semibold text-red-600">31% facing serious charges</span> such as <br/> 
+                            murder, attempted murder, or crimes <br/>  
                             against women. 
                     </p>
                 </div>
                 
-                <div className="font-gilroy text-black/50 text-xl">(Source: Association for Democratic Reforms)</div>
+                <div className="font-gilroy text-black/50 lg:text-[105%] lgMid:text-[115%] xl:text-[125%] xlMid:text-[135%] 2xl:text-[155%] 4xl:text-[170%] w-[110%] text-xl">(Source: Association for Democratic Reforms)</div>
             </div>
         </div>
     )
