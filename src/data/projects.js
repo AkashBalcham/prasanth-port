840eed8b2788d22aae9e8d11267060ca2646ffc2
Akash <akashbalcham@gmail.com>
2025-02-10 22:58:34 +0530

    add: project card images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,17 @@
-
+import vote from "../assets/images/vote.jpg"
+import transit from "../assets/images/transit.jpg"
 
 const projects = [
     {
         id: 1,
-        image: null,
+        image: transit,
         name: "Transit",
         desc: "A standalone app that integrates\nall transport modes in Bangalore\nfor seamless commuting.",
         link: "https://www.figma.com/proto/iG0HzAG7D7yiB2YP8kjdin/Portfolio?page-id=396%3A1313&node-id=396-1314&p=f&viewport=353%2C150%2C0.26&t=hYcb7GhWqfWNxskA-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=396%3A1314"
     },
     {
         id: 2,
-        image: null,
+        image: vote,
         name: "Bharat-Vote",
         desc: "Making voting easy for all with\n seamless access to informed\n choices.",
         link: "https://www.figma.com/proto/iG0HzAG7D7yiB2YP8kjdin/Portfolio?page-id=395%3A182&node-id=395-1152&viewport=673%2C-187%2C0.3&t=EpOQhJMG8SPRsHT5-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=395%3A1152"
