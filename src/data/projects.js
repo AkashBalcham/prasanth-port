03dcde80718e7d106c5ff7c780c75c05e68a18a3
Akash <akashbalcham@gmail.com>
2025-02-11 22:24:48 +0530

    fix: update figma links

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,14 +7,14 @@ const projects = [
         image: transit,
         name: "Transit",
         desc: "A standalone app that integrates\nall transport modes in Bangalore\nfor seamless commuting.",
-        link: "https://www.figma.com/proto/iG0HzAG7D7yiB2YP8kjdin/Portfolio?page-id=396%3A1313&node-id=396-1314&p=f&viewport=353%2C150%2C0.26&t=hYcb7GhWqfWNxskA-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=396%3A1314"
+        link: "https://www.figma.com/proto/OfVCj4Tbb1cGYiGhBiabK8/Transit?page-id=0%3A1&node-id=24-200&p=f&viewport=-1169%2C337%2C0.23&t=aivXO5QVgN8dq1Pl-1&scaling=scale-down&content-scaling=fixed"
     },
     {
         id: 2,
         image: vote,
         name: "Bharat-Vote",
         desc: "Making voting easy for all with\n seamless access to informed\n choices.",
-        link: "https://www.figma.com/proto/iG0HzAG7D7yiB2YP8kjdin/Portfolio?page-id=395%3A182&node-id=395-1152&viewport=673%2C-187%2C0.3&t=EpOQhJMG8SPRsHT5-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=395%3A1152"
+        link: "https://www.figma.com/proto/1E1llrq4Gvfv8nqtsPfRK6/Bharat-Vote?page-id=0%3A1&node-id=6-444&p=f&viewport=60%2C392%2C0.04&t=qpjSUqHYHwsi7egA-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=6%3A444"
     }
 ]
 
